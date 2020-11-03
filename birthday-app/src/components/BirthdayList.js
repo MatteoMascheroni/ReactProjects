d0caa9ee45992f431c9a2043c2071bd2d92b7604
Matteo Mascheroni <mascheronimatteo02@gmail.com>
2020-11-03 16:54:54 +0100

    Aggiunto stile e risolto API per foto profilo
    completare styling

--- birthday-app/src/components/BirthdayList.js
+++ birthday-app/src/components/BirthdayList.js
@@ -6,10 +6,12 @@ export default function BirthdayList(){
     const [birthday,clearAllData] = useState(data)
 
     return(
-        <div>
-            <h3>{birthday.length} birthdays today</h3>
-            {birthday.map(item => <BirthdayItem key={item.id} {...item}/>)}
-            <button onClick={()=>clearAllData([])}>Clear All</button>
-        </div>
+        <main>
+            <section>
+                <h3>{birthday.length} birthdays today</h3>
+                {birthday.map(item => <BirthdayItem key={item.id} {...item}/>)}
+                <button onClick={()=>clearAllData([])}>Clear All</button>
+            </section>
+        </main>
     )
 }
