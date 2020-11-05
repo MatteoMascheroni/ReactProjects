ab5033b19a6fe3b5618c232a8aa2edf259d13609
Matteo Mascheroni <mascheronimatteo02@gmail.com>
2020-11-05 16:03:14 +0100

    Aggiunta Loading Screen e Read More/ Show less

--- tour-app/src/App.js
+++ tour-app/src/App.js
@@ -1,16 +1,21 @@
 import React,{useState,useEffect} from "react";
 import './App.css';
 import Tour from "./Tour";
+import Loading from "./Loading";
 
 const url ="https://course-api.com/react-tours-project"
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [tours,setTours] = useState([]);
+
   const fetchTours = async() => {
     try {
       const response =  await fetch(url);
       const tours = await response.json();
+      setIsLoading(!isLoading)
       setTours(tours)
+
     } catch (error){
       console.log(error)
     }
@@ -18,8 +23,11 @@ function App() {
 
   useEffect(() => {
     fetchTours();
-  })
-  
+  },[]);
+
+  if(isLoading) {
+    return(<Loading/>)
+  }  
   return(
     <div>
       {tours.map((tour)=>(<Tour key={tour.id} {...tour}/>)) }
--- tour-app/src/Tour.js
+++ tour-app/src/Tour.js
@@ -1,11 +1,20 @@
-import React from "react"
+import React,{useState} from "react"
 
 const Tour = ({image,info,name,price}) => {
+    const [isReadMore, setIsReadMore] = useState(false);
+    const subst = (testo) => {
+        let testo_arr  = testo.split(" ");
+        return(testo_arr.slice(0,(testo.length)/2).join(" "));
+    }
     return(
         <article>
             <h4>{name}</h4>
             <img src={image} alt="error loading image"></img>
-            <p>{info}</p>
+            <p>{isReadMore ? info : `${info.split(" ").slice(0,info.split(" ").length / 2).join(" ")}...`}
+            <button onClick = {() => setIsReadMore(!isReadMore)}>
+                {!isReadMore ? "Read More" : "Show Less"}
+            </button>
+            </p>
             <p>Price: {price}</p>
         </article>
     )
